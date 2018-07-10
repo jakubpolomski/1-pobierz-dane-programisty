2b2b5bc57243257eeb08a2ec3b3609238b60bec3
Jakub Połomski <jakubpolomski@gmail.com>
2018-07-10 16:42:44 +0200

    zadanie

---
+++ main.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+
+    $('#pobierz').click(pobierzDane);
+
+    function pobierzDane() {
+
+        $.getJSON('https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php', function (data) {
+
+            $('#dane-programisty').append('<p class="name">Imię: ' + data.imie + '</p>');
+
+
+            $('#dane-programisty').append('<p class="lastname">Nazwisko: ' + data.nazwisko + '</p>');
+
+
+            $('#dane-programisty').append('<p class="occupation">Zawód: ' + data.zawod + '</p>');
+
+
+            $('#dane-programisty').append('<p class="company">Firma: ' + data.firma + '</p>');
+
+        })
+    }
+})
